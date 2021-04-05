368df2d78add12e8c888becbc6e6595daed5ea0d
zhaojie <1979170837@qq,com>
2021-04-05 16:06:22 +0800

    搭建源码阅读环境

---
+++ netty-demo/src/main/java/io/netty/demo/NettyTest.java
@@ -0,0 +1,12 @@
+package io.netty.demo;
+
+/**
+ * @author Jie Zhao
+ * @date 2021/4/5 16:05
+ */
+public class NettyTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Netty!");
+    }
+}
