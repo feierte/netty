fceb2101f7fafb9cda7854e5a53844a42ed6d607
zhaojie <1979170837@qq.com>
2021-07-16 14:27:58 +0800

    添加日志框架

--- netty-demo/src/main/java/io/netty/demo/NettyTest.java
+++ netty-demo/src/main/java/io/netty/demo/NettyTest.java
@@ -1,12 +1,16 @@
 package io.netty.demo;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author Jie Zhao
  * @date 2021/4/5 16:05
  */
+@Slf4j
 public class NettyTest {
 
     public static void main(String[] args) {
         System.out.println("Hello Netty!");
+        log.info("netty成功运行了...");
     }
 }
